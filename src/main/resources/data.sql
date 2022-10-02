7a21387e94b2259d86977ccac5ae3258be662fc8
Higashimori Daiki <bigtree1013@icloud.com>
2022-10-02 12:40:01 +0900

    DDL/DML変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO issues(summary, description) VALUES ('バグA','バグがあります');
-INSERT INTO issues(summary, description) VALUES ('機能要望B','Bに追加機能が欲しいです');
-INSERT INTO issues(summary, description) VALUES ('画面Cが遅い','早くして欲しいです');
+INSERT INTO issues(summary, description, create_datetime) VALUES ('バグA','バグがあります','2022-09-01 12:30');
+INSERT INTO issues(summary, description, create_datetime) VALUES ('機能要望B','Bに追加機能が欲しいです','2022-09-15 11:00');
+INSERT INTO issues(summary, description, create_datetime) VALUES ('画面Cが遅い','早くして欲しいです','2022-09-30 14:00');
